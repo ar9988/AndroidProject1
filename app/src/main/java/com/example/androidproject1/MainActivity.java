82c0ef965ffab48b81e9efea62899446f6c9fb01
ar9988 <80316251+ar9988@users.noreply.github.com>
2021-04-03 21:59:33 +0900

    다음,이전버튼구현

--- app/src/main/java/com/example/androidproject1/MainActivity.java
+++ app/src/main/java/com/example/androidproject1/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.androidproject1;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
@@ -24,17 +24,77 @@ public class MainActivity extends AppCompatActivity {
         int j=0;
         int year = intent.getIntExtra("year", -1);
         int month = intent.getIntExtra("month", -1);
+        int dayofweek=intent.getIntExtra("dayofweek",-1);
+        int lastday;
         if (year == -1 || month == -1) {
             year = cal.get(Calendar.YEAR);
             month = cal.get(Calendar.MONTH);
+            lastday = cal.getActualMaximum(Calendar.DATE);
+            cal.set(Calendar.DAY_OF_MONTH,1);
+            dayofweek = cal.get(Calendar.DAY_OF_WEEK); // 1=일요일 7=토요일
+        }
+        else {
+            switch (month) {
+                case 2:
+                    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                        lastday = 29;
+                    else lastday = 28;
+                    break;
+                case 4:
+                case 6:
+                case 11:
+                    lastday = 30;
+                    break;
+                default:
+                    lastday = 31;
+                    break;
+            }
         }
         TextView txt=findViewById(R.id.Text);
+        Button btn1=findViewById(R.id.Btn1);
+        Button btn2=findViewById(R.id.Btn2);
+        int finalYear = year;
+        int finalMonth = month;
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,MainActivity.class);
+                if(finalMonth==0){
+                    intent1.putExtra("year", finalYear-1);
+                    intent1.putExtra("month", 11);
+                    intent1.putExtra("dayofweek", getDayofWeek(finalYear-1,11));
+                }
+                else {
+                    intent1.putExtra("year", finalYear);
+                    intent1.putExtra("month", finalMonth-1);
+                    intent1.putExtra("dayofweek", getDayofWeek(finalYear,finalMonth-1));
+                }
+                finish();
+                startActivity(intent1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,MainActivity.class);
+                if(finalMonth==11){
+                    intent1.putExtra("year", finalYear+1);
+                    intent1.putExtra("month", 0);
+                    intent1.putExtra("dayofweek", getDayofWeek(finalYear+1,0));
+
+                }
+                else {
+                    intent1.putExtra("year", finalYear);
+                    intent1.putExtra("month", finalMonth+1);
+                    intent1.putExtra("dayofweek", getDayofWeek(finalYear,finalMonth+1));
+
+                }
+                finish();
+                startActivity(intent1);
+            }
+        });
         month+=1;
         txt.setText(year+"년 "+month+"월");
-
-        int lastday = cal.getActualMaximum(Calendar.DATE);
-        cal.set(Calendar.DAY_OF_MONTH,1);
-        int dayofweek = cal.get(Calendar.DAY_OF_WEEK); // 1=일요일 7=토요일
         // 데이터 원본 준비
         String[] items = new String[lastday+dayofweek-1];
 
@@ -49,14 +109,23 @@ public class MainActivity extends AppCompatActivity {
 
         //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<String> adapt
-                = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                items);
+                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
         // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
         GridView gridview = (GridView) findViewById(R.id.gridview);
         // 어댑터를 GridView 객체에 연결
         gridview.setAdapter(adapt);
+    }
+    public int getDayofWeek(int year,int month){
+        int []end_day= new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            end_day[1]=29;
+        year--;
+        int dayofweek=year*365+year/4-year/100+year/400;
+        for(int i=0;i<month;i++){
+            dayofweek+=end_day[i];
+        }
+        dayofweek++;
+        return dayofweek%7+1;
     }
 }
